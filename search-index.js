94eba859bdbc7e71acf9d85dbf7c1962f4bc2d4a
equation314 <equation618@gmail.com>
2024-07-17 06:05:19 +0000

    Deploying to gh-pages from @ arceos-org/handler_table@8ce678bf75460fbfbe01dd4b4dc9d533949032ba 🚀

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["handler_table",{"t":"IFNNNNNNNNNN","n":["Handler","HandlerTable","borrow","borrow_mut","from","handle","into","new","register_handler","try_from","try_into","type_id"],"q":[[0,"handler_table"],[12,"core::result"],[13,"core::any"]],"i":[0,0,3,3,3,3,3,3,3,3,3,3],"f":"``{{{b{c}}}{{b{e}}}{}{}}{{{b{dc}}}{{b{de}}}{}{}}{cc{}}{{{b{f}}h}j}{ce{}{}}{{}f}{{{b{f}}hl}j}{c{{n{e}}}{}{}}0{{{b{c}}}A`{}}","D":"A`","p":[[1,"reference"],[0,"mut"],[5,"HandlerTable",0],[1,"usize"],[1,"bool"],[8,"Handler",0],[6,"Result",12],[5,"TypeId",13]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAQAEAAAAAMABAAKAAsADAA="}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
